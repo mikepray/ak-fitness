d623483789d6935e01179db6e3413deb157b5593
Mike Pray <mwpray@gmail.com>
2023-05-29 09:13:10 -0500

    drafts, publishing, and deleting

---
+++ pages/api/auth/[...nextauth].ts
@@ -0,0 +1,21 @@
+// pages/api/auth/[...nextauth].ts
+
+import { NextApiHandler, NextApiRequest, NextApiResponse } from 'next';
+import NextAuth from 'next-auth';
+import { PrismaAdapter } from '@next-auth/prisma-adapter';
+import GitHubProvider from 'next-auth/providers/github';
+import prisma from '../../../lib/prisma';
+
+const authHandler: NextApiHandler = (req: NextApiRequest, res: NextApiResponse) => NextAuth(req, res, nextAuthOptions);
+export default authHandler;
+
+export const nextAuthOptions = {
+  providers: [
+    GitHubProvider({
+      clientId: process.env.GITHUB_ID,
+      clientSecret: process.env.GITHUB_SECRET,
+    }),
+  ],
+  adapter: PrismaAdapter(prisma),
+  secret: process.env.SECRET,
+};
