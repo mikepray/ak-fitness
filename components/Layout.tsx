3357010c899fb080a39f7cde1ce53290d171bf97
Mike Pray <mwpray@gmail.com>
2023-05-30 13:44:10 -0500

    adding mantine and refactoring header

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,50 +1,81 @@
 import React, { ReactNode } from "react";
-import Header from "./Header";
+import {
+  Anchor,
+  AppShell,
+  Button,
+  Flex,
+  Group,
+  Header,
+  Loader,
+  Navbar,
+  Text,
+  UnstyledButton,
+} from "@mantine/core";
+import { signOut, useSession } from "next-auth/react";
+import { useRouter } from "next/router";
+import { IconBarbell } from '@tabler/icons-react';
+
 
 type Props = {
   children: ReactNode;
 };
 
-const Layout: React.FC<Props> = (props) => (
-  <div>
-    <Header />
-    <div className="layout">{props.children}</div>
-    <style jsx global>{`
-      html {
-        box-sizing: border-box;
-      }
+const Layout: React.FC<Props> = (props) => {
+  const router = useRouter();
+  const isActive: (pathname: string) => boolean = (pathname) =>
+    router.pathname === pathname;
 
-      *,
-      *:before,
-      *:after {
-        box-sizing: inherit;
-      }
+  const { data: session, status } = useSession();
 
-      body {
-        margin: 0;
-        padding: 0;
-        font-size: 16px;
-        font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto,
-          Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji",
-          "Segoe UI Symbol";
-        background: rgba(0, 0, 0, 0.05);
-      }
-
-      input,
-      textarea {
-        font-size: 16px;
-      }
-
-      button {
-        cursor: pointer;
-      }
-    `}</style>
-    <style jsx>{`
-      .layout {
-        padding: 0 2rem;
-      }
-    `}</style>
-  </div>
-);
+  return (
+    <>
+      <AppShell
+        padding="md"
+        header={
+          <Header height={60} p="xs">
+            <Group position="apart">
+              <Flex align="flex-start" p="xs" gap="sm">
+                <IconBarbell />
+                <Anchor href="/" weight={isActive("/") ? "bold" : "normal"}>
+                  Feed
+                </Anchor>
+                {session && (
+                  <>
+                    <Anchor
+                      href="/drafts"
+                      weight={isActive("/drafts") ? "bold" : "normal"}
+                    >
+                      Drafts
+                    </Anchor>
+                    <Anchor
+                      href="/create"
+                      weight={isActive("/create") ? "bold" : "normal"}
+                    >
+                      Create Post
+                    </Anchor>
+                  </>
+                )}
+              </Flex>
+              <Flex align="flex-end" p="xs" gap="sm">
+                {status === "loading" && <Loader />}
+                {session && <Text>Welcome, {session?.user.name}</Text>}
+                {status !== "loading" && !session && (
+                    <Anchor href="/api/auth/signin">Log in</Anchor>
+                )}
+                {session && (
+                  <UnstyledButton onClick={() => signOut()}>
+                    <Anchor>Log Out</Anchor>
+                  </UnstyledButton>
+                )}
+              </Flex>
+            </Group>
+          </Header>
+        }
+      >
+        <div className="layout">{props.children}</div>
+      </AppShell>
+    </>
+  );
+};
 
 export default Layout;
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,21 @@
 import { SessionProvider } from 'next-auth/react';
+import { MantineProvider } from '@mantine/core';
 
 import { AppProps } from "next/app";
 
 const App = ({ Component, pageProps }: AppProps) => {
   return (
     <SessionProvider session={pageProps.session}>
+      <MantineProvider
+        withGlobalStyles
+        withNormalizeCSS
+        theme={{
+          /** Put your mantine theme override here */
+          colorScheme: 'light',
+        }}
+      >
       <Component {...pageProps} />
+      </MantineProvider>
     </SessionProvider>
   );
 };
